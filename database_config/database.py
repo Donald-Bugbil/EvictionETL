5fe34ee680666f5062cad8ea7c0352acbbb5d64c
Blaise Ayirizia <baayirizia@miners.utep.edu>
2025-07-04 21:54:12 -0400

    Third commit: Added load task to the DAG and modifying the DB credentials

--- database_config/database.py
+++ database_config/database.py
@@ -7,35 +7,37 @@ from sqlalchemy.engine import URL
 from schemas.schema import Base
 
 load_dotenv()
-#database connection
-DRIVERNAME=os.environ['DRIVERNAME']
-POSTGRES_PASSWORD=os.environ['POSTGRES_PASSWORD']
-POSTGRES_USER=os.environ['POSTGRES_USER']
-POSTGRES_DB=os.environ['POSTGRES_DB']
-HOST=os.environ['HOST']
 
-#RDS connection
-# AWS_POSTGRES_HOST=os.environ['AWS_POSTGRES_HOST']
-# AWS_POSTGRES_PORT=os.environ['AWS_POSTGRES_PORT']
-# AWS_POSTGRES_USER=os.environ['AWS_POSTGRES_USER']
-# AWS_POSTGRES_PASSWORD=os.environ['AWS_POSTGRES_PASSWORD']
-# AWS_POSTGRES_DB=os.environ['AWS_POSTGRES_DB']
+ENVIRONMENT=os.environ['ENVIRONMENT']
 
-# #database configuration
+#development connection
+if ENVIRONMENT=='development':
+
+    DRIVERNAME=os.environ['DRIVERNAME']
+    PASSWORD=os.environ['POSTGRES_PASSWORD']
+    USER=os.environ['POSTGRES_USER']
+    DB=os.environ['POSTGRES_DB']
+    HOST=os.environ['HOST']
+    PORT=os.environ['AWS_POSTGRES_PORT']
+
+
+#RDS production connection
+else:
+    DRIVERNAME=os.environ['DRIVERNAME']
+    HOST=os.environ['AWS_POSTGRES_HOST']
+    PORT=os.environ['AWS_POSTGRES_PORT']
+    USER=os.environ['AWS_POSTGRES_USER']
+    PASSWORD=os.environ['AWS_POSTGRES_PASSWORD']
+    DB=os.environ['AWS_POSTGRES_DB']
+
+#database configuration
 database_configuration=URL.create(drivername=DRIVERNAME,
-                                  username=POSTGRES_USER, 
-                                  password=POSTGRES_PASSWORD,
+                                  username=USER, 
+                                  password=PASSWORD,
                                   host=HOST,
-                                  database=POSTGRES_DB
+                                  database=DB
                                   )
 
-# database_configuration=URL.create(drivername=DRIVERNAME,
-#                                   username=AWS_POSTGRES_USER, 
-#                                   password=AWS_POSTGRES_PASSWORD,
-#                                   host=AWS_POSTGRES_HOST,
-#                                   port=AWS_POSTGRES_PORT,
-#                                   database=AWS_POSTGRES_DB
-#                                   )
 
 #start the engine
 engine=create_engine(database_configuration)
